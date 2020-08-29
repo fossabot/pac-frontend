c51607b513392feaab3bbaf06d8a2a37385c7465
Milutin Dzunic <milutindzunic@gmail.com>
2020-08-29 16:08:50 +0200

    Added inline editing of Talk title in TalkDetails component

--- src/components/talks/TalkDetails.js
+++ src/components/talks/TalkDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Link, useParams, withRouter} from "react-router-dom";
+import ContentEditable from 'react-contenteditable'
 import rest from "../../utils/rest";
 
 const TalkDetails = () => {
@@ -16,7 +17,6 @@ const TalkDetails = () => {
     const param = useParams();
 
     React.useEffect(() => {
-
         async function fetchData() {
             try {
                 const talk = await rest.doGet(`${process.env.REACT_APP_HOST}/talks/${param.id}`);
@@ -29,6 +29,19 @@ const TalkDetails = () => {
 
         fetchData();
     }, [param, error]);
+
+    const handleChange = (evt) => {
+        async function updateTalkData() {
+            try {
+                const updatedTalk = await rest.doPut(`${process.env.REACT_APP_HOST}/talks/${param.id}`, {...talk, title: evt.target.value});
+                setTalk(updatedTalk.data);
+            } catch (e) {
+                setError(e.message);
+            }
+        }
+
+        updateTalkData();
+    };
 
     const {
         title,
@@ -44,7 +57,14 @@ const TalkDetails = () => {
             <div className="row mt-5">
                 <dl className="row">
                     <dt className="col-sm-3">Talk Title</dt>
-                    <dd className="col-sm-9">{title}</dd>
+                    <dd className="col-sm-9">
+                        <ContentEditable
+                            html={title}
+                            disabled={false}
+                            onChange={handleChange}
+                            tagName=''
+                        />
+                    </dd>
 
                     <dt className="col-sm-3">Language</dt>
                     <dd className="col-sm-9">{language}</dd>
@@ -55,21 +75,21 @@ const TalkDetails = () => {
                     <dt className="col-sm-3">Persons</dt>
                     <dd className="col-sm-9">
                         {persons.map((person) =>
-                            <Link to={"/speakerDetails/" + person.id} className="card-link">{person.name}</Link>
+                            <Link key={person.id} to={"/speakerDetails/" + person.id} className="card-link">{person.name}</Link>
                         )}
                     </dd>
 
                     <dt className="col-sm-3">Topics</dt>
                     <dd className="col-sm-9">
                         {topics.map((topic) =>
-                            <span className="badge badge-info mx-1">{topic.name}</span>
+                            <span key={topic.id} className="badge badge-info mx-1">{topic.name}</span>
                         )}
                     </dd>
 
                     <dt className="col-sm-3">Events</dt>
                     <dd className="col-sm-9">
                         {talkDates.map((talkDate) =>
-                            <Link to={"/eventDetails/" + talkDate.event.id} className="card-link">{talkDate.event.name}</Link>
+                            <Link key={talkDate.id} to={"/eventDetails/" + talkDate.event.id} className="card-link">{talkDate.event.name}</Link>
                         )}
                     </dd>
                 </dl>
