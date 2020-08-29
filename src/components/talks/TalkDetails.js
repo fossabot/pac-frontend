ba3fba0c6fe52b070a4fcd8f32736ab21beddc2c
Milutin Dzunic <milutindzunic@gmail.com>
2020-08-29 13:47:58 +0200

    Refactored TalkDetails component to be functional

--- src/components/talks/TalkDetails.js
+++ src/components/talks/TalkDetails.js
@@ -1,86 +1,81 @@
 import React from 'react';
-import {Link, withRouter} from "react-router-dom";
+import {Link, useParams, withRouter} from "react-router-dom";
 import rest from "../../utils/rest";
 
-class TalkDetails extends React.Component {
-    constructor(params) {
-        super(params);
-        this.state = {
-            talk: {
-                title: "",
-                language: "",
-                level: "",
-                persons: [],
-                topics: [],
-                talkDates: [],
-            },
-        };
-    }
+const TalkDetails = () => {
+    const [talk, setTalk] = React.useState({
+        title: "",
+        language: "",
+        level: "",
+        persons: [],
+        topics: [],
+        talkDates: [],
+    });
+    const [error, setError] = React.useState("");
 
-    componentDidMount() {
-        const talkId = this.props.match.params.id;
+    const param = useParams();
 
-        rest.doGet("http://localhost:9090/talks/" + talkId)
-            .then((response) => {
-                const talk = response.data;
+    React.useEffect(() => {
 
-                this.setState({
-                    talk: talk,
-                });
-            }, (error) => {
-                console.log(error);
-            });
-    }
+        async function fetchData() {
+            try {
+                const talk = await rest.doGet(`${process.env.REACT_APP_HOST}/talks/${param.id}`);
 
-    render() {
+                setTalk(talk.data);
+            } catch (e) {
+                setError(e.message);
+            }
+        }
 
-        const {
-            title,
-            language,
-            level,
-            persons,
-            topics,
-            talkDates,
-        } = this.state.talk;
+        fetchData();
+    }, [param, error]);
 
-        return (
-            <div className="container">
-                <div className="row mt-5">
-                    <dl className="row">
-                        <dt className="col-sm-3">Talk Title</dt>
-                        <dd className="col-sm-9">{title}</dd>
+    const {
+        title,
+        language,
+        level,
+        persons,
+        topics,
+        talkDates,
+    } = talk;
 
-                        <dt className="col-sm-3">Language</dt>
-                        <dd className="col-sm-9">{language}</dd>
+    return (
+        <div className="container">
+            <div className="row mt-5">
+                <dl className="row">
+                    <dt className="col-sm-3">Talk Title</dt>
+                    <dd className="col-sm-9">{title}</dd>
 
-                        <dt className="col-sm-3">Level</dt>
-                        <dd className="col-sm-9">{level}</dd>
+                    <dt className="col-sm-3">Language</dt>
+                    <dd className="col-sm-9">{language}</dd>
 
-                        <dt className="col-sm-3">Persons</dt>
-                        <dd className="col-sm-9">
-                            {persons.map((person) =>
-                                <Link to={"/speakerDetails/" + person.id} className="card-link">{person.name}</Link>
-                            )}
-                        </dd>
+                    <dt className="col-sm-3">Level</dt>
+                    <dd className="col-sm-9">{level}</dd>
 
-                        <dt className="col-sm-3">Topics</dt>
-                        <dd className="col-sm-9">
-                            {topics.map((topic) =>
-                                <span className="badge badge-info mx-1">{topic.name}</span>
-                            )}
-                        </dd>
+                    <dt className="col-sm-3">Persons</dt>
+                    <dd className="col-sm-9">
+                        {persons.map((person) =>
+                            <Link to={"/speakerDetails/" + person.id} className="card-link">{person.name}</Link>
+                        )}
+                    </dd>
 
-                        <dt className="col-sm-3">Events</dt>
-                        <dd className="col-sm-9">
-                            {talkDates.map((talkDate) =>
-                                <Link to={"/eventDetails/" + talkDate.event.id} className="card-link">{talkDate.event.name}</Link>
-                            )}
-                        </dd>
-                    </dl>
-                </div>
+                    <dt className="col-sm-3">Topics</dt>
+                    <dd className="col-sm-9">
+                        {topics.map((topic) =>
+                            <span className="badge badge-info mx-1">{topic.name}</span>
+                        )}
+                    </dd>
+
+                    <dt className="col-sm-3">Events</dt>
+                    <dd className="col-sm-9">
+                        {talkDates.map((talkDate) =>
+                            <Link to={"/eventDetails/" + talkDate.event.id} className="card-link">{talkDate.event.name}</Link>
+                        )}
+                    </dd>
+                </dl>
             </div>
-        );
-    }
+        </div>
+    );
 }
 
 export default withRouter(TalkDetails);
