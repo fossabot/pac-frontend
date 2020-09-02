b82c5a3369bfed040697c04dd838e4cd2ecb507d
Milutin Dzunic <milutin.dzunic@prodyna.com>
2020-09-02 10:50:45 +0200

    Initial adding of keycloak config - not working
    :

--- src/index.js
+++ src/index.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {InfoProvider} from "./components/context";
 import * as serviceWorker from './serviceWorker';
+import Keycloak from 'keycloak-js'
 
-ReactDOM.render(
-    <React.StrictMode>
-        <InfoProvider>
+debugger;
+
+if (true) {
+    //keycloak init options
+    let initOptions = {
+        url: "http://conference.keycloak/auth", realm: "PAC", clientId: "pac-frontend", onLoad: 'login-required'
+    }
+
+    let keycloak = Keycloak(initOptions);
+    keycloak.init({ onLoad: initOptions.onLoad }).success((auth) => {
+
+        if (!auth) {
+            window.location.reload();
+        } else {
+            console.info("Authenticated");
+        }
+
+        //React Render
+        ReactDOM.render(
+            <React.StrictMode>
+                <App/>
+            </React.StrictMode>,
+            document.getElementById('root')
+        );
+
+        localStorage.setItem("react-token", keycloak.token);
+        localStorage.setItem("react-refresh-token", keycloak.refreshToken);
+
+        setTimeout(() => {
+            keycloak.updateToken(70).success((refreshed) => {
+                if (refreshed) {
+                    console.debug('Token refreshed' + refreshed);
+                } else {
+                    console.warn('Token not refreshed, valid for '
+                        + Math.round(keycloak.tokenParsed.exp + keycloak.timeSkew - new Date().getTime() / 1000) + ' seconds');
+                }
+            }).error(() => {
+                console.error('Failed to refresh token');
+            });
+
+        }, 60000)
+
+    }).error(() => {
+        console.error("Authenticated Failed");
+    });
+} else {
+    ReactDOM.render(
+        <React.StrictMode>
             <App/>
-        </InfoProvider>
-    </React.StrictMode>,
-    document.getElementById('root')
-);
+        </React.StrictMode>,
+        document.getElementById('root')
+    );
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
